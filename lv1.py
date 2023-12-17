54d0d0c044c13ceaa1a22fb8030773191229a5a4
trungvo347 <strungvo@gmail.com>
2023-12-17 11:57:09 +0700

    Trung

--- lv1.py
+++ lv1.py
@@ -1,23 +1,9 @@
+import character
 import turtle
 
-screnn = turtle.Screen
-player= turtle.Turtle()
-player.speed(0)
-player.penup()
-playerSpeed = 30 
 
-def move_left() :
-    x = player.xcor() - playerSpeed
-    player.setx(x)
-    
-def move_right() :
-    x = player.xcor() - playerSpeed
-    player.setx(x)
-    
-def move_up() :
-    y = player.ycor() - playerSpeed
-    player.sety(y)
-    
-def move_down() :
-    y = player.ycor() - playerSpeed
-    player.set(y)
+turtle.listen()
+turtle.onkey(character.move_left, 'Left')
+turtle.onkey(character.move_right, 'Right')
+turtle.onkey(character.move_up, 'Up')
+turtle.onkey(character.move_down, 'Down')
