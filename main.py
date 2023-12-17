4fefc492e69666ace1762262c453ec706b86a186
minh1734 <153408754+minh1734@users.noreply.github.com>
2023-12-16 20:10:29 -0800

    minh

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+screen = turtle.Screen()
+player = turtle.Turtle()
+screen.setup(213,236)
+screen.bgpic('C:\\Users\\LVTINH\\Documents\\background.gif')
+screen.update()
+screen.addshape("C:\\Users\\LVTINH\\Documents\\ZF6W.gif")
+player.shape("C:\\Users\\LVTINH\\Documents\\ZF6W.gif")
+
+screen.update()
+
+
+screen.exitonclick()
