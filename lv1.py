e66e5871c0561e52378860c75e467dd93cdb3ff0
trungvo347 <strungvo@gmail.com>
2023-12-17 11:54:02 +0700

    trung

---
+++ lv1.py
@@ -0,0 +1,23 @@
+import turtle
+
+screnn = turtle.Screen
+player= turtle.Turtle()
+player.speed(0)
+player.penup()
+playerSpeed = 30 
+
+def move_left() :
+    x = player.xcor() - playerSpeed
+    player.setx(x)
+    
+def move_right() :
+    x = player.xcor() - playerSpeed
+    player.setx(x)
+    
+def move_up() :
+    y = player.ycor() - playerSpeed
+    player.sety(y)
+    
+def move_down() :
+    y = player.ycor() - playerSpeed
+    player.set(y)
